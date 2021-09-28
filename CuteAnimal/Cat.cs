7b0196e4b1bacaf87ba2813589a7dac9c95c0d45
DanielPinhao22007445 <daniel2pinhao@gmail.com>
2021-09-28 12:43:00 +0100

    added Cat

--- CuteAnimal/Cat.cs
+++ CuteAnimal/Cat.cs
@@ -2,6 +2,14 @@ namespace CuteAnimal
 {
     public class Cat
     {
-        
+        private string name;
+        private int energy;
+        private Feed feed;
+        private Mood mood;
+
+        public void Eat;
+        public void Sleep;
+        public void Meow;
+        public void Play;
     }
 }
